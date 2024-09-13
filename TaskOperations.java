0a55d3d7c5441412b290ffb38764a2fea8063b77
Suhani Mathur <139855595+suhani2706@users.noreply.github.com>
2024-09-13 12:19:16 +0530

    Changes made

--- TaskOperations.java
+++ TaskOperations.java
@@ -12,62 +12,69 @@ import java.util.Scanner;
 public class TaskOperations {
 	
 	//Task 1 :Count Palindrome
-	private static boolean isPal(String s, int i, int j) {
-        if ( i >= j ) {
-            return true;
-        }
-        if ( s.charAt(i) != s.charAt(j) ) {
-            return false;
-        }
-        return isPal( s, i + 1, j - 1 );
-    }
+	 private static boolean isPal(String string, int i, int j) {
+	        if (i >= j) {
+	            return true;
+	        }
+	        if (string.charAt(i) != string.charAt(j)) {
+	            return false;
+	        }
+	        return isPal(string, i + 1, j - 1);
+	    }
 
-    // Recursive method to find palindrome substrings and count unique ones
-    private static int countPalindromicSubstrings( String s, int start, int end, String[] palindromes, int count ) {
-        if ( start >= s.length() ) {
-            return count;
-        }
-        if ( end >= s.length() ) {
-            return countPalindromicSubstrings( s, start + 1, start + 1, palindromes, count );
-        }
+	    private static int countPalindromicSubstrings(String inputString, int start, int end, int count) {
+	        if (start >= inputString.length()) {
+	            return count;
+	        }
+	        if (end >= inputString.length()) {
+	            return countPalindromicSubstrings(inputString, start + 1, start + 1, count);
+	        }
 
-        if ( isPal(s, start, end) ) {
-            String substring = s.substring(start, end + 1);
-            if (!isAlreadyCounted(palindromes, count, substring)) {
-                palindromes[count] = substring;
-                count++;
-            }
-        }
-        return countPalindromicSubstrings(s, start, end + 1, palindromes, count);
-    }
+	        if (isPal(inputString, start, end)) {
+	            String substring = inputString.substring(start, end + 1);
+	            if (!isAlreadyCounted(inputString, start, end)) {
+	                count++;
+	            }
+	        }
+	        return countPalindromicSubstrings(inputString, start, end + 1, count);
+	    }
 
-    // Method to check if a substring is already counted
-    private static boolean isAlreadyCounted(String[] palindromes, int count, String substring) {
-        for (int i = 0; i < count; i++) {
-            if (palindromes[i].equals(substring)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    private static int countPS(String s) {
-        // Array to hold all palindromic substrings
-        String[] palindromes = new String[s.length() * (s.length() + 1) / 2];
-        return countPalindromicSubstrings(s, 0, 0, palindromes, 0);
-    }    
-    public static void CountPalindrome() {
-    	Scanner sc = new Scanner(System.in);
-    	System.out.println(Constants.Task1);
-    	String PalindromeString = sc.nextLine();
-    	
-    	if(PalindromeString.equals("exit")) {
-    		System.out.println("Exiting the program");
-    		return;
-    	}
-    	int result = countPS(PalindromeString);
-    	System.out.println("Number of palindrome in the string:" + result);
-    	CountPalindrome();
-    }
+	    private static boolean isAlreadyCounted(String inputString, int start, int end) {
+	        for (int i = 0; i < start; i++) {
+	            for (int j = i; j < end; j++) {
+	                if (isPal(inputString, i, j) && inputString.substring(i, j + 1).equals(inputString.substring(start, end + 1))) {
+	                    return true;
+	                }
+	            }
+	        }
+	        return false;
+	    }
+
+	    private static int countPS(String s) {
+	        return countPalindromicSubstrings(s, 0, 0, 0);
+	    }
+
+	    public static void CountPalindrome() {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter a string (max 80 characters) or type 'exit' to quit:");
+
+	        String PalindromeString = sc.nextLine();
+
+	        if (PalindromeString.equalsIgnoreCase("exit")) {
+	            System.out.println("Exiting the program");
+	            return;
+	        }
+
+	        if (PalindromeString.length() > 80) {
+	            System.out.println("Input string exceeds 80 characters.");
+	            CountPalindrome();
+	            
+	        }
+
+	        long result = countPS(PalindromeString);
+	        System.out.println("Number of palindromic substrings in the string: " + result);
+	        CountPalindrome();
+	    }
     
     
     
@@ -122,11 +129,11 @@ public class TaskOperations {
             return;
            }
             try{
-            int n = Integer.parseInt(input);
-            long[] memo = new long[n + 1];
-            System.out.println("The " + n + "th Fibonacci number is: " + fibonacci(n, memo));
+            int number = Integer.parseInt(input);
+            long[] memo = new long[number + 1];
+            System.out.println("The " + number + "th Fibonacci number is: " + fibonacci(number, memo));
             }
-            catch(Exception e){
+            catch(Exception ex){
                 System.out.println(Constants.Task2_invalid);
             }
             nthfibonacci();
@@ -149,28 +156,37 @@ public class TaskOperations {
         
         // Task 3 : Snake To Camel Case
         public static void CamelAndSnakeCase() {
-        	Scanner scanner = new Scanner(System.in);
-        	
+            Scanner scanner = new Scanner(System.in);
+        
             System.out.println(Constants.Task3);
-            
+        
             String input = scanner.nextLine();
-           if(input.equals("exit")){
-        	   System.out.println("Exiting the code...");
-        	   return;
-           }
-            // Convert input to snake_case and then to camelCase
-            String snakeCase = toSnakeCase(input, 0);
-            String camelCase = toCamelCase(snakeCase, 0, 0);
-
+            if (input.equals("exit")) {
+                System.out.println("Exiting the code...");
+                return;
+            }
+            String camelCase;
+            if (input.contains("_")) {
+                camelCase = toCamelCase(input, 0, 0, true);  
+            } else {
+                String snakeCase = toSnakeCase(input, 0);
+                camelCase = toCamelCase(snakeCase, 0, 0, true); 
+            }
+        
             System.out.println("CamelCase version: " + camelCase);
-            CamelAndSnakeCase();            
+            CamelAndSnakeCase();
         }
-     // Convert camelCase to snake_case
+        
+        // Convert camelCase to snake_case
         private static String toSnakeCase(String str, int index) {
             if (index >= str.length()) {
                 return "";
             }
             char currentChar = str.charAt(index);
+            if (!Character.isLetter(currentChar)) {
+                return toSnakeCase(str, index + 1);  
+            }
+         
             if (Character.isUpperCase(currentChar)) {
                 return "_" + Character.toLowerCase(currentChar) + toSnakeCase(str, index + 1);
             } else if (currentChar == ' ') {
@@ -179,44 +195,60 @@ public class TaskOperations {
                 return currentChar + toSnakeCase(str, index + 1);
             }
         }
+        
         // Convert snake_case to camelCase
-        private static String toCamelCase(String str, int index, int capitalizeNext) {
+        private static String toCamelCase(String str, int index, int capitalizeNext, boolean isFirstWord) {
             if (index >= str.length()) {
                 return "";
             }
             char currentChar = str.charAt(index);
+            
             if (currentChar == '_') {
-                return toCamelCase(str, index + 1, 1);
-            } else if (capitalizeNext == 1) {
-                return Character.toUpperCase(currentChar) + toCamelCase(str, index + 1, 0);
-            } else {
-                return currentChar + toCamelCase(str, index + 1, 0);
+                return toCamelCase(str, index + 1, 1, isFirstWord);
             }
+           
+            if (isFirstWord) {
+                return Character.toLowerCase(currentChar) + toCamelCase(str, index + 1, 0, false);
+            }
+            
+            if (capitalizeNext == 1) {
+                return Character.toUpperCase(currentChar) + toCamelCase(str, index + 1, 0, false);
+            }
+            
+            if (!Character.isLetter(currentChar) && currentChar != '_') {
+                return toCamelCase(str, index + 1, capitalizeNext, isFirstWord);  
+            }
+            
+            return currentChar + toCamelCase(str, index + 1, 0, false);
         }
+        
+        
+        
         
         
         
         //Task 4: Count Consonants
         private static final char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        
 
-        private static boolean isVowel(char c, int index) {
+        private static boolean isVowel(char character, int index) {
             if (index == vowels.length) {
                 return false;
             }
-            if (c == vowels[index]) {
+            if (character == vowels[index]) {
                 return true;
             }
-            return isVowel(c, index + 1);
+            return isVowel(character, index + 1);
         }
-        private static boolean isConsonant(char c) {
-            return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') && !isVowel(c, 0);
+        private static boolean isConsonant(char character) {
+            return (character >= 'a' && character <= 'z' || character >= 'A' && character <= 'Z') && !isVowel(character, 0);
         }
-        private static int countingConsonants(String str, int index) {
-            if (index == str.length()) {
+        private static int countingConsonants(String string, int index) {
+            if (index == string.length()) {
                 return 0;
             }
-            int count = isConsonant(str.charAt(index)) ? 1 : 0;
-            return count + countingConsonants(str, index + 1);
+            int count = isConsonant(string.charAt(index)) ? 1 : 0;
+            return count + countingConsonants(string, index + 1);
         }
         public static void countConsonants() {
             Scanner sc = new Scanner(System.in);
@@ -231,8 +263,6 @@ public class TaskOperations {
             System.out.println("Number of consonants: " + result);            
             countConsonants();  
         } 
-        public static void Exiting() {
-        	
-        }
+        
 }
 
